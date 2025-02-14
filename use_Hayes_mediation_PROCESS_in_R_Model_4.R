b931ded7334a10ddbdf13cb36e674a7d2b037456
Jonathan Carvalho <108154259+jocapsi@users.noreply.github.com>
2025-02-13 23:29:53 -0300

    Update use_Hayes_mediation_PROCESS_in_R_Model_4.R

--- use_Hayes_mediation_PROCESS_in_R_Model_4.R
+++ use_Hayes_mediation_PROCESS_in_R_Model_4.R
@@ -1,9 +1,3 @@
-#Notes:
-#1. The author accepts no responsibility for the topicality, correctness, completeness, or quality of the information provided.
-#2. This pdf is part of a YouTube tutorial: https://youtu.be/4Aq3RRRu0DI 
-#3. This pdf is for your own personal use only. Please do not distribute further.
-
-
 # Read the data
 data_mediation <-
   read.csv("https://raw.githubusercontent.com/tidydatayt/mediation_analysis/m
